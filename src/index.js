420b1dab66f7ff6b428175b6f0a966f287284540
Atanas Atanasov <naso.003@gmail.com>
2020-12-29 01:39:15 +0200

    added entry form

--- src/index.js
+++ src/index.js
@@ -2,146 +2,61 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Square(props) {
-    return (
-        <button className="square" onClick={props.onClick}>
-            {props.value}
-        </button>
-    );    
-}
-  
-class Board extends React.Component {
-    renderSquare(i) {
-        return (
-        <Square 
-            value={this.props.squares[i]}
-            onClick={() => this.props.onClick(i)}
-        />
-        );
-    }
-
-    render() {
-        return (
-        <div>
-            <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-            </div>
-            <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-            </div>
-            <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-            </div>
-        </div>
-        );
-    }
-}
-
-class Game extends React.Component {
+class EntryForm extends React.Component {
     constructor(props) {
-        super(props);
-        this.state= {
-            history: [{
-                squares: Array(9).fill(null)
-            }],
-            stepNumber: 0,
-            xIsNext: true
-        };
+      super(props);
+      this.state = {value: '', "playerInfo": ''};
+
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {    
+        this.setState({value: event.target.value});  
     }
 
-    handleClick(i) {
-        const history = this.state.history.slice(0, this.state.stepNumber + 1);
-        const current = history[history.length - 1];
-        const squares = current.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            history: history.concat([{
-                squares: squares
-            }]),
-            stepNumber: history.length,
-            xIsNext: !this.state.xIsNext
+    handleSubmit(event) {
+        const proxyUrl = 'https://ancient-depths-46233.herokuapp.com/';
+        let url = proxyUrl + 'https://fantasy.premierleague.com/api/entry/' + this.state.value + '/';
+
+        fetch(url, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let playerInfo = ` ${data.name}, ${data.player_first_name} ${data.player_last_name}, ${data.player_region_name}`;
+            this.setState({playerInfo: playerInfo});
         });
-    }
 
-    jumpTo(step) {
-        this.setState({
-            stepNumber: step,
-            xIsNext: (step % 2) === 0
-        });
+        event.preventDefault();
     }
-
+  
     render() {
-        const history = this.state.history;
-        const current = history[this.state.stepNumber];
-        const winner = calculateWinner(current.squares);
-
-        const moves = history.map((step, move) => {
-            const desc = move ? 
-            'Go to move #' + move :
-            'Go to game start';
-            return (
-                <li key={move}>
-                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
-                </li>
-            );
-        });
-
-        let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-
-        return (
-        <div className="game">
-            <div className="game-board">
-            <Board 
-                squares={current.squares}
-                onClick={(i) => this.handleClick(i)}
-            />
-            </div>
-            <div className="game-info">
-            <div>{status}</div>
-            <ol>{moves}</ol>
-            </div>
+      return (
+        <div className="app">
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Player ID:
+                    <input 
+                        type="text" 
+                        placeholder="e.g. 2458458"
+                        value={this.state.value} 
+                        onChange={this.handleChange} />        
+                </label>
+                <input type="submit" value="Check" />
+            </form>
+            <div>{this.state.playerInfo}</div>
         </div>
-        );
+      );
     }
-}
+  }
   
 // ========================================
   
 ReactDOM.render(
-    <Game />,
+    <EntryForm />,
     document.getElementById('root')
 );
-
-function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
