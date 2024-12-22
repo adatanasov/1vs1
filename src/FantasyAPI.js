22c957c6ff6355f72ad6a800f743fcdf7855e874
Atanas Atanasov <naso.003@gmail.com>
2024-12-23 01:20:31 +0200

    check player fixtures

--- src/FantasyAPI.js
+++ src/FantasyAPI.js
@@ -1,6 +1,8 @@
 const proxyUrl = 'https://cryptic-plains-87247.herokuapp.com/';
 const apiBaseUrl = 'https://fantasy.premierleague.com/api';
+const pulseApiBaseUrl = 'https://footballapi.pulselive.com';
 const url = `${proxyUrl}${apiBaseUrl}`;
+const pulseUrl = `${proxyUrl}${pulseApiBaseUrl}`;
 
 export async function getEntryById(id) {
     return fetch(`${url}/entry/${id}/`)
@@ -73,5 +75,34 @@ export async function getPlayerPicksForEvent(playerId, event) {
     .then(response => response.json())
     .then(data => {
         return data;
+    });
+}
+
+export async function getFixturePlayers(fixtureId) {
+    return fetch(`${pulseUrl}/football/fixtures/${fixtureId}?altIds=true&fast=false`)
+    .then(response => response.json())
+    .then(data => {       
+        let pickIds = [];
+        let lineUpAvailable = 
+            data.teamLists 
+            && data.teamLists.length === 2 
+            && data.teamLists[0] 
+            && data.teamLists[0].lineup 
+            && data.teamLists[0].lineup.some(f => f) 
+            && data.teamLists[0].substitutes 
+            && data.teamLists[0].substitutes.some(f => f) 
+            && data.teamLists[1] 
+            && data.teamLists[1].lineup 
+            && data.teamLists[1].lineup.some(f => f) 
+            && data.teamLists[1].substitutes 
+            && data.teamLists[1].substitutes.some(f => f);
+        if (lineUpAvailable) {                
+            pickIds = 
+                [...data.teamLists[0].lineup.map(p => p.altIds.opta), 
+                ...data.teamLists[0].substitutes.map(p => p.altIds.opta), 
+                ...data.teamLists[1].lineup.map(p => p.altIds.opta), 
+                ...data.teamLists[1].substitutes.map(p => p.altIds.opta)];
+        }
+        return pickIds;
     });
 }
