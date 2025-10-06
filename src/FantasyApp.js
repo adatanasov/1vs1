9dff46660a1cc46f7870c1e71036377e29625f81
Atanas Atanasov <naso.003@gmail.com>
2025-10-07 00:16:59 +0300

    updat ereact to 19.2

--- src/FantasyApp.js
+++ src/FantasyApp.js
@@ -178,7 +178,7 @@ class FantasyApp extends Component {
                     secondaryColor="#37003C"
                     wrapperClass="loader" />
                 {!this.state.selectedLeague &&
-                    <div className="version">v.1.58</div>}
+                    <div className="version">v.1.59</div>}
                 {!this.state.playerId && <EntryForm />}
                 {this.state.playerId && this.state.playerName && 
                     <PlayerName 
