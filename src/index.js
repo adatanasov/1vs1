4bb29eea494dc6769529ed1714f4e14e09e22559
Atanas Atanasov <naso.003@gmail.com>
2025-10-07 23:26:56 +0300

    remove usage for cross-env

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import { createRoot } from 'react-dom/client';
-import DraftApp from './DraftApp';
 import FantasyApp from './FantasyApp';
 import './assets/index.css';
   
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-const setup = process.env.REACT_APP_SETUP;
-if (setup === 'draft') {
-    root.render(<DraftApp />);
-} else {
-    root.render(<FantasyApp />);
-}
+root.render(<FantasyApp />);
